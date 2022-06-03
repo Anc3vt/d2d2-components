6c4160973c9ac1d9ce9dee1ed484e3eac4c50638
Ancevt <i@ancevt.ru>
2022-06-03 10:32:23 +0300

    ScrollPane

---
+++ src/main/java/com/ancevt/d2d2/components/ScrollPane.java
@@ -0,0 +1,197 @@
+/**
+ * Copyright (C) 2022 the original author or authors.
+ * See the notice.md file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ancevt.d2d2.components;
+
+import com.ancevt.d2d2.common.PlainRect;
+import com.ancevt.d2d2.event.Event;
+import com.ancevt.d2d2.event.InteractiveEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScrollPane extends Component {
+
+    private static final float DEFAULT_WIDTH = 300.0f;
+    private static final float DEFAULT_HEIGHT = 300.0f;
+    private static final float DEFAULT_ITEM_HEIGHT = 30.0f;
+
+    public static final int DEFAULT_SCROLL_STEP = 4;
+
+    private final PlainRect bg;
+    private final Scrollbar scrollbar;
+    private final List<Component> components;
+
+    private int scrollPosition;
+    private int scrollStep;
+
+    private float itemHeight;
+
+    public ScrollPane() {
+        bg = new PlainRect(BACKGROUND_COLOR);
+        bg.setAlpha(PANEL_BG_ALPHA);
+        add(bg);
+
+        scrollStep = DEFAULT_SCROLL_STEP;
+
+        components = new ArrayList<>();
+
+        setItemHeight(DEFAULT_ITEM_HEIGHT);
+
+        scrollbar = new Scrollbar();
+        scrollbar.addEventListener(ScrollPane.class, Event.CHANGE, this::scrollbar_change);
+        add(scrollbar);
+
+        addEventListener(ScrollPane.class, Event.RESIZE, this::this_resize);
+        addEventListener(ScrollPane.class, InteractiveEvent.WHEEL, this::this_wheel);
+
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        setFocusRectEnabled(false);
+    }
+
+    private void this_resize(Event event) {
+        bg.setSize(getWidth(), getHeight());
+        scrollbar.setX(getWidth() - scrollbar.getWidth());
+        scrollbar.setHeight(getHeight());
+        rebuild();
+    }
+
+    private void scrollbar_change(Event event) {
+        int position = (int) (components.size() * scrollbar.getScrollValue());
+        setScrollPosition(position);
+    }
+
+    private void this_wheel(Event event) {
+        var e = (InteractiveEvent) event;
+        scroll(-e.getDelta() * scrollStep);
+    }
+
+    public void setBackgroundVisible(boolean backgroundVisible) {
+        bg.setVisible(backgroundVisible);
+    }
+
+    public boolean isBackgroundVisible() {
+        return bg.isVisible();
+    }
+
+    public void setScrollStep(int scrollStep) {
+        this.scrollStep = scrollStep;
+    }
+
+    public int getScrollStep() {
+        return scrollStep;
+    }
+
+    public void scroll(int step) {
+        setScrollPosition(getScrollPosition() + step);
+
+        float pos = scrollPosition;
+        float count = components.size();
+
+        float value = pos / (count - getHeight() / itemHeight);
+
+        scrollbar.setScrollValue(value);
+    }
+
+    public void setItemHeight(float itemHeight) {
+        this.itemHeight = itemHeight;
+    }
+
+    public float getItemHeight() {
+        return itemHeight;
+    }
+
+    public void addScrollableItem(Component component) {
+        components.add(component);
+        component.removeEventListener(ScrollPane.class, Event.REMOVE);
+        component.addEventListener(ScrollPane.class, Event.REMOVE, event -> {
+            focus();
+        });
+        rebuild();
+    }
+
+    public void removeScrollableItem(Component component) {
+        components.remove(component);
+        rebuild();
+    }
+
+    public void removeScrollableItem(int index) {
+        components.remove(index).removeFromParent();
+        rebuild();
+    }
+
+    public Component getScrollableItem(int index) {
+        return components.get(index);
+    }
+
+    public int getScrollableItemCount() {
+        return components.size();
+    }
+
+    public float getTotalScrollableHeight() {
+        return components.size() * itemHeight;
+    }
+
+    public void setScrollPosition(int scrollPosition) {
+        this.scrollPosition = scrollPosition;
+
+        if (this.scrollPosition < 0) {
+            this.scrollPosition = 0;
+        }
+
+        int max = (int) (components.size() - getHeight() / itemHeight);
+        if (this.scrollPosition >= max) {
+            this.scrollPosition = max;
+        }
+
+        rebuild();
+    }
+
+    public int getScrollPosition() {
+        return scrollPosition;
+    }
+
+    @Override
+    void update() {
+        rebuild();
+    }
+
+    private void rebuild() {
+        components.forEach(Component::removeFromParent);
+
+        float y = 0.0f;
+        for (int i = scrollPosition; y < getHeight() && i < components.size(); i++) {
+            Component item = components.get(i);
+            add(item, 0, y);
+            item.move(getPadding().getLeft(), getPadding().getTop());
+            item.setSize(
+                    getWidth() - getPadding().getRight() - getPadding().getLeft(),
+                    getItemHeight() - getPadding().getBottom() - getPadding().getTop()
+            );
+            y += itemHeight;
+        }
+
+        float val = getHeight() / getTotalScrollableHeight();
+
+        scrollbar.removeFromParent();
+        if (val < 1.0f) {
+            add(scrollbar);
+        }
+
+        scrollbar.setRectLength(getHeight() * val);
+    }
+}
---
+++ src/main/java/com/ancevt/d2d2/components/Scrollbar.java
@@ -0,0 +1,238 @@
+/**
+ * Copyright (C) 2022 the original author or authors.
+ * See the notice.md file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ancevt.d2d2.components;
+
+import com.ancevt.d2d2.D2D2;
+import com.ancevt.d2d2.backend.lwjgl.LWJGLBackend;
+import com.ancevt.d2d2.common.PlainRect;
+import com.ancevt.d2d2.debug.DebugPanel;
+import com.ancevt.d2d2.display.Color;
+import com.ancevt.d2d2.display.Stage;
+import com.ancevt.d2d2.display.texture.Texture;
+import com.ancevt.d2d2.event.Event;
+import com.ancevt.d2d2.event.InteractiveEvent;
+import com.ancevt.d2d2.interactive.InteractiveSprite;
+
+public class Scrollbar extends Component {
+
+    private static final float DEFAULT_WIDTH = 6.0f;
+    private static final float DEFAULT_HEIGHT = 200.0f;
+
+    private static final float MIN_ALPHA = 0.1f;
+    private static final float MAX_ALPHA = 1.0f;
+    private static final float ALPHA_SPEED = 0.1f;
+    public static final int FADE_HOLD = 50;
+
+    private final PlainRect _debugRect;
+    private final ScrollbarRect rect;
+    private int fadeHold = FADE_HOLD;
+    private boolean hovered;
+
+    public Scrollbar() {
+        setTabbingEnabled(false);
+
+        _debugRect = new PlainRect();
+        _debugRect.setColor(Color.GRAY);
+        //add(_debugRect);
+
+        rect = new ScrollbarRect(this);
+        rect.setColor(Color.WHITE);
+        add(rect);
+
+        addEventListener(Scrollbar.class, Event.RESIZE, this::this_resize);
+        addEventListener(Scrollbar.class, InteractiveEvent.HOVER, this::this_hover);
+        addEventListener(Scrollbar.class, InteractiveEvent.OUT, this::this_out);
+        addEventListener(Scrollbar.class, InteractiveEvent.WHEEL, this::this_wheel);
+
+        setAlpha(MIN_ALPHA);
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+    private void this_wheel(Event event) {
+        var e = (InteractiveEvent) event;
+        scroll(-e.getDelta());
+    }
+
+    private void scroll(int delta) {
+        rect.moveY(delta * 25);
+        rect.fixBounds();
+        dispatchEvent(Event.builder().type(Event.CHANGE).build());
+        fadeIn();
+    }
+
+    public void setScrollValue(float scrollValue) {
+        if (scrollValue == getScrollValue()) return;
+        if (scrollValue < 0f) {
+            scrollValue = 0f;
+        } else if (scrollValue > 1.0f) {
+            scrollValue = 1.0f;
+        }
+        rect.setY(scrollValue * (getHeight() - rect.getHeight()));
+        fadeIn();
+    }
+
+    public float getScrollValue() {
+        return rect.getY() / (getHeight() - rect.getHeight());
+    }
+
+    @Override
+    public void onEachFrame() {
+        super.onEachFrame();
+
+        if (fadeHold > 0) {
+            setAlpha(getAlpha() + ALPHA_SPEED);
+            if (getAlpha() >= MAX_ALPHA) setAlpha(MAX_ALPHA);
+        } else {
+            if (getAlpha() > MIN_ALPHA) {
+                setAlpha(getAlpha() - ALPHA_SPEED);
+                if (getAlpha() <= MIN_ALPHA) setAlpha(MIN_ALPHA);
+            }
+        }
+
+        if (fadeHold > 0 && !hovered) fadeHold--;
+    }
+
+    private void fadeIn() {
+        fadeHold = FADE_HOLD;
+    }
+
+    private void this_out(Event event) {
+        hovered = false;
+    }
+
+    private void this_hover(Event event) {
+        fadeIn();
+        hovered = true;
+    }
+
+    private void this_resize(Event event) {
+        _debugRect.setSize(getWidth(), getHeight());
+        rect.setWidth(getWidth());
+    }
+
+    @Override
+    void update() {
+
+    }
+
+    @Override
+    public void dispose() {
+        rect.dispose();
+        super.dispose();
+    }
+
+    public void setRectLength(float rectLength) {
+        rect.setHeight(rectLength);
+    }
+
+    public float getRectLength() {
+        return rect.getHeight();
+    }
+
+    private static class ScrollbarRect extends InteractiveSprite {
+
+        private static final float DEFAULT_HEIGHT = 60.0f;
+        private static final String ASSET_1X1 = "d2d2-core-1x1.png";
+        private static Texture texture;
+
+        private final Scrollbar scrollbar;
+
+        private int oldY;
+
+        private static Texture get1x1Texture() {
+            if (texture != null) return texture;
+            return texture = D2D2.getTextureManager().loadTextureAtlas(ASSET_1X1).createTexture();
+        }
+
+        public ScrollbarRect(Scrollbar scrollbar) {
+            super(get1x1Texture());
+            this.scrollbar = scrollbar;
+            setHeight(DEFAULT_HEIGHT);
+
+            setPushEventUp(false);
+
+            addEventListener(ScrollbarRect.class, InteractiveEvent.DOWN, this::this_down);
+            addEventListener(ScrollbarRect.class, InteractiveEvent.DRAG, this::this_drag);
+            addEventListener(ScrollbarRect.class, InteractiveEvent.WHEEL, scrollbar::dispatchEvent);
+        }
+
+        private void this_down(Event event) {
+            var e = (InteractiveEvent) event;
+            oldY = (int) (e.getY() + getY());
+        }
+
+        private void this_drag(Event event) {
+            var e = (InteractiveEvent) event;
+            final int ty = (int) (e.getY() + getY());
+            moveY(ty - oldY);
+            oldY = ty;
+            fixBounds();
+            scrollbar.fadeIn();
+            scrollbar.dispatchEvent(Event.builder().type(Event.CHANGE).build());
+        }
+
+        private void fixBounds() {
+            if (getY() < 0) {
+                setY(0);
+            } else if (getY() > scrollbar.getHeight() - getHeight()) {
+                setY(scrollbar.getHeight() - getHeight());
+            }
+        }
+
+        @Override
+        public float getWidth() {
+            return getScaleX();
+        }
+
+        @Override
+        public float getHeight() {
+            return getScaleY();
+        }
+
+        public void setSize(float width, float height) {
+            setScale(width, height);
+        }
+
+        public void setWidth(float width) {
+            setScaleX(width);
+        }
+
+        public void setHeight(float height) {
+            setScaleY(height);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Stage stage = D2D2.init(new LWJGLBackend(800, 600, "(floating)"));
+        stage.setBackgroundColor(Color.of(0x001122));
+
+        Scrollbar scrollbar = new Scrollbar();
+
+        stage.add(scrollbar, 100, 100);
+
+        DebugPanel.setEnabled(true);
+        DebugPanel.show("test", "").ifPresent(debugPanel -> {
+            debugPanel.addButton("-0.1", () -> scrollbar.setScrollValue(scrollbar.getScrollValue() - 0.1f));
+            debugPanel.addButton("+0.1", () -> scrollbar.setScrollValue(scrollbar.getScrollValue() + 0.1f));
+        });
+
+        D2D2.loop();
+    }
+
+}
